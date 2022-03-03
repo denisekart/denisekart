da61b28c7ffec73b7712800af8465281d7b786eb
Denis Ekart <denis.ekart@gmail.com>
2022-03-03 20:48:49 +0100

    feat: added developer excuse generator

--- README-template.md
+++ README-template.md
@@ -1,14 +1,20 @@
-﻿### Hi, I'm Denis ✌️
+### Hi, I'm Denis ✌️
+
 #### I'm a Lead Software Engineer @ 13|37
+
 ![Lead Software Engineer @ 13|37](https://oss-tools.1337.services/readmemaker/img/Logotype-dark.svg)
 
 I am an innovative programmer with close to a decade of professional experience in software design and development. I thrive in a team environment and love solving complex problems. I have extensive knowledge and understanding of the .NET ecosystem including strong experience in Microsoft Azure.
 
-#### Hey, here's fun developer excuse you can use
+---
 
-*It’s never shown unexpected behavior like this before.*
+#### Hey, here's fun developer excuse you can use (you didn't get it from me)
+
+*@@excuse@@*
 
 > You can get a new excuse every hour, just keep refreshing 😁
+
+---
 
 Skills:  ![.NET](https://img.shields.io/badge/-.NET-05e273)  ![C#](https://img.shields.io/badge/-C%23-05e273)  ![JS](https://img.shields.io/badge/-JS-05e273)  ![Powershell](https://img.shields.io/badge/-Powershell-05e273)  ![ASP.NET](https://img.shields.io/badge/-ASP.NET-05e273)  ![Azure](https://img.shields.io/badge/-Azure-05e273)  ![Docker](https://img.shields.io/badge/-Docker-05e273)  ![CI](https://img.shields.io/badge/-CI-05e273)  ![CD](https://img.shields.io/badge/-CD-05e273)  ![Cypress.io](https://img.shields.io/badge/-Cypress.io-05e273)  ![DevExpress](https://img.shields.io/badge/-DevExpress-05e273)  ![EF Core](https://img.shields.io/badge/-EF%20Core-05e273)  ![DDD](https://img.shields.io/badge/-DDD-05e273)  ![ELK](https://img.shields.io/badge/-ELK-05e273)  ![OpenID Connect](https://img.shields.io/badge/-OpenID%20Connect-05e273)  ![Git](https://img.shields.io/badge/-Git-05e273)  ![LINQ](https://img.shields.io/badge/-LINQ-05e273)  ![Node.js](https://img.shields.io/badge/-Node.js-05e273)  ![xUnit](https://img.shields.io/badge/-xUnit-05e273)  ![NUnit](https://img.shields.io/badge/-NUnit-05e273)  ![SpecFlow](https://img.shields.io/badge/-SpecFlow-05e273)  ![Blazor](https://img.shields.io/badge/-Blazor-05e273)  ![MassTransit](https://img.shields.io/badge/-MassTransit-05e273)  ![Redis](https://img.shields.io/badge/-Redis-05e273)  ![SignalR](https://img.shields.io/badge/-SignalR-05e273)  ![SQL](https://img.shields.io/badge/-SQL-05e273)  ![WPF](https://img.shields.io/badge/-WPF-05e273)  ![Quartz.net](https://img.shields.io/badge/-Quartz.net-05e273)  ![gRPC](https://img.shields.io/badge/-gRPC-05e273)
 
---
+++ build/Build.cs
@@ -0,0 +1,76 @@
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
+using Nuke.Common.Tooling;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Serilog.Log;
+
+[GitHubActions("excuses",
+    GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = true,
+    OnCronSchedule = "0 * * * *",
+    CacheIncludePatterns = new[] { "~/.nuget/packages" },
+    ImportSecrets = new[] { "NUKE_EXCUSES_GET_ENDPOINT", "NUKE_EXCUSES_REGEX" },
+    InvokedTargets = new[] { nameof(NewExcuse) },
+    OnWorkflowDispatchOptionalInputs = new[] { "DUMMY_NAME" }
+    )]
+class Build : NukeBuild
+{
+    const string ExcuseToken = "@@excuse@@";
+
+    [GitRepository] readonly GitRepository GitRepository;
+
+    [PathExecutable] readonly Tool Git;
+
+    [Parameter] readonly string ExcusesGetEndpoint;
+    [Parameter] readonly string ExcusesRegex;
+
+    public static int Main() => Execute<Build>();
+
+    Target NewExcuse => _ => _
+        .Requires(() => ExcusesGetEndpoint, () => ExcusesRegex)
+        .Executes(async () =>
+        {
+            Information("Getting a new excuse...");
+            var excuse = await GetExcuse();
+            Information("Getting content of README-template.md...");
+            var template = await GetTemplateContent();
+            Information("Generating new README.md from template...");
+            var interpolatedContent = IntepolateTemplateWithExcuse(template, excuse, ExcuseToken);
+            Information("Replacing old README.md...");
+            await ReplaceReadme(interpolatedContent);
+            Information("Commiting changes...");
+            CommitChanges();
+            Information("ALL DONE!");
+        });
+
+    private Task ReplaceReadme(string interpolatedContent) => File.WriteAllTextAsync(RootDirectory / "README.md", interpolatedContent, Encoding.UTF8);
+    private string IntepolateTemplateWithExcuse(string template, string excuse, string excuseToken) => template.Replace(excuseToken, excuse);
+    private Task<string> GetTemplateContent() => File.ReadAllTextAsync(RootDirectory / "README-template.md");
+    private async Task<string> GetExcuse()
+    {
+        var client = new HttpClient();
+        var response = await client.GetStringAsync(ExcusesGetEndpoint);
+
+        var match = Regex.Match(response, ExcusesRegex);
+        if (!match.Success) throw new Exception("Expected a match in regex but found none");
+        var firstCapture = match.Groups.Values.Skip(1).FirstOrDefault()?.Value;
+        if (firstCapture == null) throw new Exception("Expexted a first capture group to return a match but no match was found");
+
+        return firstCapture;
+    }
+    private void CommitChanges()
+    {
+        Git("config --global user.name \"excuse bot [bot]\"");
+        Git("config --global user.email \"denis.ekart@gmail.com\"");
+        Git("add -A");
+        Git("commit -m \"[bot] chore: another day, another excuse\"");
+        Git("push");
+    }
+}
